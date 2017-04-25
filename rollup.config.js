e7045ea56efb35444170f0e9614f41ce45d033e4
Nick Schot <nickschot@gmail.com>
2017-04-25 15:10:07 +0200

    Remove babel Add example async function

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,10 @@
-import babel from 'rollup-plugin-babel';
-import babelrc from 'babelrc-rollup';
-import istanbul from 'rollup-plugin-istanbul';
-
 let pkg = require('./package.json');
 let external = Object.keys(pkg.dependencies);
 
 export default {
+  dest: 'dist/index.js',
   entry: 'lib/index.js',
-  plugins: [
-    babel(babelrc()),
-    istanbul({
-      exclude: ['test/**/*', 'node_modules/**/*']
-    })
-  ],
-  external: external,
-  targets: [
-    {
-      dest: pkg.main,
-      format: 'umd',
-      moduleName: 'rollupStarterProject',
-      sourceMap: true
-    },
-    {
-      dest: pkg.module,
-      format: 'es',
-      sourceMap: true
-    }
-  ]
+  format: 'cjs',
+  plugins: [],
+  external: external
 };
